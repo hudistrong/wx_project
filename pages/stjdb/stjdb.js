afe37b61b523e5dc1b1120b11459e3e79eeb5c86
胡笛 <hud@tongxinyiliao.com>
2016-11-23 16:44:26 +0800

    石头剪刀布

---
+++ pages/stjdb/stjdb.js
@@ -0,0 +1,57 @@
+Page({
+    data: {
+        arr: [1,2,3],
+        n:0,
+        m:1,
+        win: 0,
+        lose: 0,
+        hiddenModal: true,
+        result_text: false,
+        image_tag: ["4777971", "4777970", "4777969"]
+    },
+    getStart: function(eventTarget) {
+        var arr = [1,2,3];
+        var m = eventTarget.currentTarget.dataset.hi;
+        var n = Math.floor(Math.random() * arr.length + 1)-1;
+        var win = this.data.win;
+        var lose = this.data.lose;
+        this.setData({n: n});
+        this.setData({m: m});
+        if(m!=arr[n]){
+            if (m==1){
+                if(arr[n]==3){
+                    this.setData({hiddenModal: false, result_text: false, lose: lose+1})
+                }else if(arr[n]==2){
+                    this.setData({hiddenModal: false, result_text: true, win: win+1})
+                }
+            }else if (m==2){
+                if(arr[n]==1){
+                    this.setData({hiddenModal: false, result_text: false, lose: lose+1})
+                }else if(arr[n]==3){
+                    this.setData({hiddenModal: false, result_text: true, win: win+1})
+                }
+            }else if (m==3){
+                if(arr[n]==2){
+                    this.setData({hiddenModal: false, result_text: false, lose: lose+1})
+                }else if(arr[n]==1){
+                    this.setData({hiddenModal: false, result_text: true, win: win+1})
+                }
+            }
+        }
+    },
+    listenerButton:function(){
+        this.setData({
+            hiddenModal: !this.data.hiddenModal
+        })
+    },
+    listenerConfirm:function() {
+        this.setData({
+            hiddenModal: true
+        })
+    },
+    listenerCancel:function() {
+        this.setData({
+            hiddenModal: true
+        })
+    },
+});
